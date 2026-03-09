dddeea0ef0a4903ecec1297e86c9feb951757a87
Perfecto <perfectogardoce@gmail.com>
2026-03-09 12:50:27 +0800

    finished the registration logic

---
+++ simple-crud-concept/app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $incommingFields = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $incommingFields['password'] = bcrypt($incommingFields['password']);
+        User::create($incommingFields);
+
+        return "Thank you for registering";
+    }
+}
